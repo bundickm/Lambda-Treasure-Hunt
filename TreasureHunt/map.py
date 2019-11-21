847f925261e8811f689b8f14359c74adf2b4285c
bundickm <bundickm@gmail.com>
2019-11-21 01:59:39 -0800

    Moved work out of notebook, still need to mine

--- TreasureHunt/map.py
+++ TreasureHunt/map.py
@@ -12,45 +12,32 @@ rooms = make_blank_map()
 sleep(data['cooldown']+1)
 
 while rooms_visited < 500:
-  current_room_id = data['room_id']
-  room_data = extract_room_info(data)
+    current_room_id = data['room_id']
+    room_data = extract_room_info(data)
 
-  for key in room_data:
-    rooms[current_room_id][key] = room_data[key]
+    for key in room_data:
+        rooms[current_room_id][key] = room_data[key]
 
-  error = None
-  while (data['items'] != []) and (not error):
-    data = get_item({'name': data['items'][0]})
-    error = data['errors']
-    if not error:
-      print('Picked up item')
+    data = shop_check(data)
+    data = item_check(data)
+
+    exits = data['exits']
+    valid_moves = find_unvisited_directions(exits, rooms[current_room_id])
+
+    if valid_moves != []:
+        move = choice(valid_moves)
+        data = make_move({'direction': move,
+                          "next_room_id": str(map[str(current_room_id)][move])})
+        backtrack.append(reverse[move])
+        rooms[current_room_id][move] = data['room_id']
+        rooms[data['room_id']][reverse[move]] = current_room_id
+        rooms_visited += 1
+        print(rooms_visited)
+    else:
+        move = backtrack.pop(-1)
+        data = make_move({'direction': move,
+                          'next_room_id': str(map[str(current_room_id)][move])})
+
     sleep(data['cooldown']+1)
-
-  if data['title'] is 'Shop':
-    inventory = get_status()['inventory']
-    for item in inventory:
-      data = sell_item({'name': item, 'confirm': 'yes'})
-      print('Sold Item')
-      sleep(data['cooldown']+1)
-    print('All items sold')
-
-  exits = data['exits']
-  valid_moves = find_unvisited_directions(exits, rooms[current_room_id])
-
-  if valid_moves != []:
-    move = choice(valid_moves)
-    data = make_move({'direction': move,
-                      "next_room_id": str(map[str(current_room_id)][move])})
-    backtrack.append(reverse[move])
-    rooms[current_room_id][move] = data['room_id']
-    rooms[data['room_id']][reverse[move]] = current_room_id
-    rooms_visited += 1
-    print(rooms_visited)
-  else:
-    move = backtrack.pop(-1)
-    data = make_move({'direction': move,
-                      'next_room_id': str(map[str(current_room_id)][move])})
-
-  sleep(data['cooldown']+1)
 
 json_to_file(rooms, 'full_map.txt')
